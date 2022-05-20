5e81a26a19ad6c1136d84d2c0e8de85edc77f58e
Rahim <rahim.nagori@gmail.com>
2022-05-20 10:40:13 +0530

    Configuration

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/job/';
-$config['PROJECT'] = 'Pambron Healthcare Limited';
+$config['base_url'] = 'http://localhost/agile-staffing/';
+$config['PROJECT'] = 'Agile Staffing';
 
 /*
 |--------------------------------------------------------------------------
